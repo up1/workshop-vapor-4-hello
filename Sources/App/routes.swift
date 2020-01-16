ae16c0c337d56460693f36b53ad008dad9e917e2
up1 <somkiat.p@gmail.com>
2020-01-16 14:03:06 +0800

    Hello with path variable

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,6 +9,21 @@ func routes(_ app: Application) throws {
     app.get("hello") { req in
         return "Hello, world!"
     }
+    
+    // Route to /hello/workshop
+    app.get("hello", "workshop") { req in
+        return "Hello Vapor Workshop!"
+    }
+    
+    // Working with path variable from URL
+    app.get("beers", ":count") { request -> String in
+        guard let count =
+            request.parameters.get("count", as: Int.self) else {
+                throw Abort(.badRequest)
+        }
+        return "There were \(count) beers on the wall"
+    }
+    
 
     let todoController = TodoController()
     app.get("todos", use: todoController.index)
