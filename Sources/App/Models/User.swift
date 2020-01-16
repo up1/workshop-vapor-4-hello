594430172294c41384671f3dd58c464f5e372c2f
up1 <somkiat.p@gmail.com>
2020-01-16 14:47:04 +0800

    Working with fluent

---
+++ Sources/App/Models/User.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  
+//
+//  Created by Somkiat Puisungnoen on 16/1/2563 BE.
+//
+
+import Vapor
+import Fluent
+
+final class User: Model, Content {
+    static let schema: "users"
+    
+    @ID(key: "id") var id: Int?
+    @Field(key: "name") var name: String
+    @Field(key: "username") var username: String
+    
+    init() { }
+    init(id: Int? = nil, name: String, username: String) {
+        self.id = id
+        self.name = name
+        self.username = username
+    }
+    
+}
