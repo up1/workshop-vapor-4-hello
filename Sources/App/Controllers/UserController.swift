fb4afcbaf4d100c2a62cd3aa7b774428e2b5755f
up1 <somkiat.p@gmail.com>
2020-01-16 16:01:50 +0800

    Quiz 3 :: get user by id

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -12,6 +12,7 @@ struct UserController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         routes.post("api", "user", use: createHandler)
         routes.get("api", "user", use: getAllHandler)
+        routes.get("api", "user", ":id", use: getById)
     }
     
     func createHandler(request: Request) throws ->EventLoopFuture<User> {
@@ -21,5 +22,13 @@ struct UserController: RouteCollection {
     
     func getAllHandler(request: Request) throws ->EventLoopFuture<[User]> {
         User.query(on: request.db).all()
+    }
+    
+    func getById(request: Request) throws ->EventLoopFuture<User> {
+        guard let id =
+            request.parameters.get("id", as: Int.self) else {
+                throw Abort(.badRequest)
+        }
+        return User.find(id, on: request.db).unwrap(or: Abort(.notFound))
     }
 }
