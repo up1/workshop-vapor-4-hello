8fbcaa580c84401a5034318f0d24f288f94d0151
up1 <somkiat.p@gmail.com>
2020-01-16 14:10:08 +0800

    Quiz 1

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -16,12 +16,22 @@ func routes(_ app: Application) throws {
     }
     
     // Working with path variable from URL
+    // /beers/:count
     app.get("beers", ":count") { request -> String in
         guard let count =
             request.parameters.get("count", as: Int.self) else {
                 throw Abort(.badRequest)
         }
         return "There were \(count) beers on the wall"
+    }
+    
+    // Quiz 1 :: /hello/:name
+    app.get("hello", ":name") { request -> String in
+        guard let name =
+            request.parameters.get("name") else {
+                throw Abort(.badRequest)
+        }
+        return "Hello \(name)"
     }
     
 
