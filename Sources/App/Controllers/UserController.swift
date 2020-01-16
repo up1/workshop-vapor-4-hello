1ea6214726c9cfcd393d348288639806736f9138
up1 <somkiat.p@gmail.com>
2020-01-16 15:53:44 +0800

    Create new route to get all user /api/user

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -11,10 +11,15 @@ import Fluent
 struct UserController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         routes.post("api", "user", use: createHandler)
+        routes.get("api", "user", use: getAllHandler)
     }
     
     func createHandler(request: Request) throws ->EventLoopFuture<User> {
         let user = try request.content.decode(User.self)
         return user.save(on: request.db).map {user}
+    }
+    
+    func getAllHandler(request: Request) throws ->EventLoopFuture<[User]> {
+        User.query(on: request.db).all()
     }
 }
